93637be3e3f4e9013f6ac42a8621f32a14586b50
Sanassytoure360 <touressanassy9@gmail.com>
2026-05-24 13:44:16 +0000

    third commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#Score obtained by the student
+English = 50
+Maths = 60
+Geography = 56
+Science = 70
+Economics = 13
+print("English:", English)
+print("Maths:", Maths)
+print("Geography:", Geography)
+print("Science:", Science)
+print("Economics:", Economics)
+#Calculate the total score
+total_score= English + Maths + Geography + Science + Economics
+#calculate the average_score
+average_score = total_score / 5
+print("total_score:", total_score)
+print("average_score:", average_score)
+
+
+
+
+
