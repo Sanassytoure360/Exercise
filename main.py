f9617d6f65f2a7e63522370f0201f73a8e95d27f
Sanassytoure360 <touressanassy9@gmail.com>
2026-05-30 00:29:36 +0000

    Fourth commit

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 #Score obtained by the student
-English = 50
-Maths = 60
-Geography = 56
-Science = 70
-Economics = 13
-print("English:", English)
-print("Maths:", Maths)
-print("Geography:", Geography)
-print("Science:", Science)
-print("Economics:", Economics)
+#English = 50
+#Maths = 60
+#Geography = 56
+#Science = 70
+#Economics = 13
+#print("English:", English)
+#print("Maths:", Maths)
+#print("Geography:", Geography)
+#print("Science:", Science)
+#print("Economics:", Economics)
 #Calculate the total score
-total_score= English + Maths + Geography + Science + Economics
+#total_score= English + Maths + Geography + Science + Economics
 #calculate the average_score
-average_score = total_score / 5
-print("total_score:", total_score)
-print("average_score:", average_score)
+#average_score = total_score / 5
+#print("total_score:", total_score)
+#print("average_score:", average_score)
+
+
+Text = ("Python is a powerful programming language that is easy to learn and fun to use."
+"It lets you work quickly and integrate systems more effectively."
+"Learning Python can open doors to data science, web development, and artificial intelligence.")
+MYVAR = Text.upper()
+print(MYVAR)
+
+text = "Python is a powerful language that is easy to learn and fun to use, it lets you work quicly and integrate systems more effectively, learning Python can open doors to data science, web development, and artificial intelligence."
+print(text.replace("Python", "JAVA"))
+
+MyVariableName = Text
+print(MyVariableName)
+
+Passage = ("Python is a powerful programming language that is easy to learn and fun to use."
+"It lets you work quickly and integrate systems more effectively."
+"Learning Python can open doors to data science, web development, and artificial intelligence.")
+
+Passage = Passage.split()
+print(Passage)
+X = [Passage]
+X = len(Passage)
+print(X)
+
+
+
 
 
 
