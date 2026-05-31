90e3253955db7100e1c1cacf4b822cb9ba37d1a6
Sanassytoure360 <touressanassy9@gmail.com>
2026-05-31 13:43:37 +0000

    Election

--- main.py
+++ main.py
@@ -38,47 +38,51 @@
 # X = [Passage]
 # X = len(Passage)
 # print(X)
-
-Text= """In the modern world, technology is evolving faster than ever before. Many professionals are turning to programming languages to automate their daily tasks. Python has become the most popular choice for beginners and experts alike because its syntax is clean and readable."
-"With tools like Pandas for data manipulation, Matplotlib for data visualization, and Scikit-Learn for machine learning, the possibilities are endless. Are you ready to begin your journey? If you invest just thirty minutes a day, you will see massive progress. Start small, stay consistent, and build something amazing today!"""
-count = Text.count("data")
-print(count)
-my_var = Text.lower()
-print (my_var)
-x = Text
-new_text = x.replace("!",".")
-print(new_text)
-y = Text
-Outcome = "Java" in Text
-print(Outcome)
-count = y.count(" ")
-print(count)
-List = Text.split(".")
-print(List)
-
-List = Text.split(".")
-
-New_list =[]
-
-for item in List:
-    item = item.strip()
-    if item != " " :New_list.append(item + "?")
-print(New_list)
-
-S = Text
-clean = S.replace("?" and ".", "").replace("!",".")
-
-words = clean.split()
-first_word = words[0]
-last_word = words[-1]
-print(first_word)
-print(last_word)
-
-S = Text
-Replace = S.replace("clean and readable","intuitive and powerful")
-print(Replace)
+#Text= """In the modern world, technology is evolving faster than ever before. Many professionals are turning to programming languages to automate their daily tasks. Python has become the most popular choice for beginners and experts alike because its syntax is clean and readable."
+#"With tools like Pandas for data manipulation, Matplotlib for data visualization, and Scikit-Learn for machine learning, the possibilities are endless. Are you ready to begin your journey? If you invest just thirty minutes a day, you will see massive progress. Start small, stay consistent, and build something amazing today!"""
+#count = Text.count("data")
+#print(count)
+#my_var = Text.lower()
+#rint (my_var)
+#x = Text
+#new_text = x.replace("!",".")
+#print(new_text)
+#y = Text
+#Outcome = "Java" in Text
+#print(Outcome)
+#count = y.count(" ")
+#print(count)
+#List = Text.split(".")
+#print(List)
+#List = Text.split(".")
+#New_list =[]
+#for item in List:
+ #   item = item.strip()
+  #  if item != " " :New_list.append(item + "?")
+#print(New_list)
+#S = Text
+#clean = S.replace("?" and ".", "").replace("!",".")
+#words = clean.split()
+#first_word = words[0]
+#last_word = words[-1]
+#print(first_word)
+#print(last_word)
+#S = Text
+#Replace = S.replace("clean and readable","intuitive and powerful")
+#print(Replace)
 
 
+name = input("Enter your name: ").upper()
+age = int(input("Enter your Age: "))
+gender = input("Enter your gender: ").lower()
+Outcome = "Eligible" if age>=18 else "Not eligible"
+print (Outcome)
+if gender in ["male", "female"]:
+    print("name:", name)
+    print("gender:", gender)
+    print ("Registration successful")
+else:
+    print("Invalid gender")
 
 
 
